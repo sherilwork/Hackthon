bcbe3e090e8992f59d8245de87a7130f367dff68
Sheril <sherilworks.in@gmail.com>
2026-07-09 06:33:04 +0000

    remove  (_for element <Header>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Header } from '@/components/landing/Header';
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-background selection:bg-primary/20">
-      <Header />
-      
-      <main className="pt-32">
+      <main>
         {/* Content removed as requested */}
       </main>
     </div>
